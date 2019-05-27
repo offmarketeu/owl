95ed1f8be6a53ff42fa746fbc01d1464954af7ba
offmarketeu <analisis@offmarket.eu>
2019-05-27 10:53:17 +0200

    Add files via upload

--- app_pre.R
+++ app_pre.R
@@ -18,7 +18,8 @@ ui <- fluidPage(
   
   
   headerPanel(
-    img(src="descarga.jpg",height=50)
+    list(img(src="descarga.jpg",height=50),
+    img(src="logo.png",height=50))
   ),
   titlePanel("OWL-EC Control"),
   
@@ -94,6 +95,7 @@ ui <- fluidPage(
                  
                  
         ),
+        ##############################################################
         tabPanel("DTAs",
                  h3(),
                  h3(textOutput("bu_dta")), 
@@ -108,6 +110,7 @@ ui <- fluidPage(
                  
                  
         ), 
+        ##############################################################
         tabPanel("Fondo Comercio",
                  h3(),
                  h3(textOutput("bu_gw")), 
@@ -125,18 +128,36 @@ ui <- fluidPage(
         ##############################################################
         tabPanel("Mercado",
                  h3(),
-                 h3(textOutput("bu_alm")), 
+                 h3(textOutput("bu_mkt")), 
                  h3(),
                  h3("Capital"),
                  h3(),
-                 tableOutput("alm"),
+                 tableOutput("mkt"),
                  h3(),
                  h3("P&L"),
                  h3(),
-                 h3(textOutput("Criteria")),
-                 plotOutput("plot_alm1")
+                 #uiOutput("mkt2"),
+                 h3(),
+                 plotOutput("mkt2")
+                 
                  
         ),
+        ##############################################################
+        tabPanel("Negocio",
+                 h3(),
+                 h3(textOutput("bu_ng")),
+                 h3(),
+                 h3("Capital"),
+                 h3(),
+                 tableOutput("ng"),
+                 h3(),
+                 h3("Capital - Promedio Historico"),
+                 h3(),
+                 tableOutput("ng_h"),
+                 h3(),
+                 h3("Saldos"),
+                 h3(),
+                 tableOutput("neg1"))
         
       )
     )
@@ -385,7 +406,7 @@ server <- function(input, output) {
   output$bu_mkt<- reactive({input$buvert})
   
   output$mkt<- renderTable({
-    res<-dbSendQuery(con, paste0("SELECT SUM(EC_DW_RTU), SUM(REPORTING_V), SUM(EC_DW_TOTAL) FROM ADDON WHERE BU_VERT='", input$buvert , "' AND fecha='", fcal() , "' AND TIPO_RIESGO='ALM' GROUP BY BU_VERT", sep=""))
+    res<-dbSendQuery(con, paste0("SELECT INSTRUMENTO_LOCAL, SUM(EC_DW_RTU), SUM(REPORTING_V), SUM(EC_DW_TOTAL) FROM ADDON WHERE BU_VERT='", input$buvert , "' AND fecha='", fcal() , "' AND TIPO_RIESGO='Market' GROUP BY BU_VERT, INSTRUMENTO_LOCAL", sep=""))
     result<-dbFetch(res)          
     format(result, big.mark=".")
   })
@@ -404,13 +425,68 @@ server <- function(input, output) {
     tempo_mkt<- as.data.frame(result)
   })
   
-  output$plot_mkt2 <-  renderIU({
-    Crit<- unique(tempo_mkt()$Criterias)
-    for (i in crit) {
-      tempo_mkt2<- tempo_mkt()[Criterias==i,]  
-      ggplot(tempo_mkt2, aes(x=fdata, y=PNL,  group = 1))+ geom_line(color = "red")
-    }
+  #output$mkt2 <-  renderUI({
+  #  crit<- unique(tempo_mkt()$Criterias)
+  #  for (i in crit) {
+  #    tempo_mkt2<- tempo_mkt()[Criterias==i,]  
+  #    ggplot(tempo_mkt2, aes(x=fdata, y=PNL,  group = 1))+ geom_line(color = "red")
+  #  }
+  #})
+  
+  output$mkt2<- renderPlot({
+    ggplot(tempo_mkt(), aes(x=fdata, y=PNL,colour=CRITERIA,group=2))+ geom_line()
     
+  })
+  
+  
+  ####################################################
+  # Negocio
+  # Capital
+  ####################################################
+  
+  output$bu_ng<- reactive({input$buvert})
+  output$ng<- renderTable({
+    res<-dbSendQuery(con, paste0("SELECT SUM(EC_DW_RTU),  SUM(REPORTING_V), SUM(EC_DW_TOTAL) FROM ADDON WHERE BU_VERT='", input$buvert , "' AND fecha='", fcal(), "' AND TIPO_RIESGO='Business' GROUP BY BU_VERT", sep=""))
+    result<-dbFetch(res)
+    format(result, big.mark=".", justify =c("right"))
+  })
+  
+  ####################################################
+  # Activos Materiales
+  # Capital - historico
+  ####################################################
+  
+  #output$am_h<- renderTable({
+  #  res<-dbSendQuery(con, paste0("SELECT EC_DW_RTU,  REPORTING_V, EC_DW_TOTAL FROM ADDON WHERE BU_VERT='", input$buvert , "' AND fecha LIKE '%", finit() ,"%' AND TIPO_RIESGO='Activos Materiales' GROUP BY BU_VERT", sep=""))
+  #  result<-dbFetch(res)
+  
+  #})
+  
+  
+  
+  ####################################################
+  # Saldos
+  ####################################################
+  tempo_am<- reactive({res<-dbSendQuery(con, paste0("SELECT RU_ID FROM ADDON WHERE BU_VERT='", input$buvert , "' AND fecha='", fcal(), "' AND TIPO_RIESGO='Business' GROUP BY BU_VERT,fecha", sep=""))
+  result<-dbFetch(res)
+  format(result, big.mark=".")
+  tempo_am<- as.data.frame(result)
+  })
+  
+  
+  
+  tempo1_am<- reactive({res<-dbSendQuery(con, paste0("SELECT U_CdG, Importe_2, COD_DIV1, RU_ID_NEGOCIO FROM PYG WHERE EPIGRAFE='101104' OR EPIGRAFE='1010901' AND fecha='" , fcal() , "'", sep=""))
+  result1<-dbFetch(res)
+  format(result1, big.mark=".")
+  tempo1_am<- as.data.frame(result1)
+  
+  })
+  
+  #output$tec<- renderTable({left_join(tempo_am(), tempo1_am(), by=c("RU_ID"="RU_ID_ACTIVOS_MATERIALES"))})
+  
+  output$tec<- renderTable({res<-dbSendQuery(con, paste0("SELECT PYG.U_CdG, PYG.EPIGRAFE,sum(PYG.Importe_2), PYG.COD_DIV1, PYG.RU_ID_NEGOCIO FROM PYG,ADDON WHERE PYG.EPIGRAFE IN('101104','1010901') AND PYG.fecha='" , fcal() , "' AND ADDON.BU_VERT='", input$buvert ,"' AND PYG.RU_ID_ACTIVOS_MATERIALES=ADDON.RU_ID GROUP BY PYG.U_CdG,PYG.EPIGRAFE,PYG.COD_DIV1, PYG.RU_ID_NEGOCIO" , sep=""))
+  result2<-dbFetch(res)
+  format(result2, big.mark=".")
   })
   
   
